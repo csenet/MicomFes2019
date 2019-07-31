d4fcfa5c870ab89558a5d96553f0a10d7e1ccaa1
kouichihirachi <kouichi.hirachi@gmail.com>
2019-07-31 21:06:59 +0900

    First Commit

---
+++ WebRTCTest/script.js
@@ -0,0 +1,28 @@
+let localStream = null;
+let peer = null;
+let existingCall = null;
+
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(function (stream) {
+        // Success
+        $('#my-video').get(0).srcObject = stream;
+        localStream = stream;
+    }).catch(function (error) {
+        // Error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
+peer = new Peer({
+  ket: '',
+  debug : 3
+});
+peer.on('open',function(){
+  $('#my-id').text(peer.id);
+});
+peer.on('error',function(err){
+  console.log(err.message);
+});
+peer.on('close',function(){
+});
+peer.on('disconnected',function(){
+});
