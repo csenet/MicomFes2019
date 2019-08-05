f1eec75575ff53e63e680512dd9a39baf5600eb2
kouichihirachi <kouichi.hirachi@gmail.com>
2019-08-05 18:28:38 +0900

    画面共有実装

--- script.js
+++ script.js
@@ -1,101 +1,117 @@
-let localStream = null;
-let peer = null;
-let existingCall = null;
-var screenshare = ScreenShare.create({ debug: true });
+$(function() {
+  let localStream = null;
+  let peer = null;
 
-screenshare.start({
-})
-.then(function(stream){
-  $('#my-video').get(0).srcObject = stream;
-  localStream = stream;
-})
-.catch(function(error){
-  console.error('mediaDevice.getUserMedia() error:', error);
-  return;
-});
-var result = screenshare.isScreenShareAvailable();
-alert(result);
-/*
-let media = navigator.mediaDevices.getUserMedia({
-  video: {
-    mandatory: {
-      choromeMediaSource: 'desktop',
-      choromeMediaSourceId: ''
-    }
-  },
-  video: true,
-  audio: true
-});
+  let existingCall = null;
+  const screenshare = ScreenShare.create({
+    debug: true
+  });
+  if (screenshare.isScreenShareAvailable() === false) {
+    alert('Screen Share cannot be used. Please install the Chrome extension.');
+    return;
+  }
+  screenshare.start({
+      width: $('#Width').val(),
+      height: $('#Height').val(),
+      frameRate: $('#FrameRate').val(),
+    })
+    .then(stream => {
+      $('#my-video')[0].srcObject = stream;
 
-media.then(function(stream) {
-  // Success
-  $('#my-video').get(0).srcObject = stream;
-  localStream = stream;
-});
-media.catch(function(error) {
-  // Error
-  console.error('mediaDevice.getUserMedia() error:', error);
-  return;
-});
-*/
-peer = new Peer({
-  key: '4852b666-0601-4aaf-b74c-1e099ce0860a',
-  debug: 3
-});
-peer.on('open', function() {
-  $('#my-id').text(peer.id);
-});
-peer.on('error', function(err) {
-  console.log(err.message);
-});
-peer.on('disconnected', function() {});
+      if (existingCall !== null) {
+        const peerid = existingCall.peer;
+        existingCall.close();
+        const call = peer.call(peerid, stream);
+        step3(call);
+      }
+      localStream = stream;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  var result = screenshare.isScreenShareAvailable();
+  /*
+  let media = navigator.mediaDevices.getUserMedia({
+    video: {
+      mandatory: {
+        choromeMediaSource: 'desktop',
+        choromeMediaSourceId: ''
+      }
+    },
+    video: true,
+    audio: true
+  });
 
-function addVideo(call, stream) {
-  $('#their-video').get(0).srcObject = stream;
-}
+  media.then(function(stream) {
+    // Success
+    $('#my-video').get(0).srcObject = stream;
+    localStream = stream;
+  });
+  media.catch(function(error) {
+    // Error
+    console.error('mediaDevice.getUserMedia() error:', error);
+    return;
+  });
+  */
+  peer = new Peer({
+    key: '4852b666-0601-4aaf-b74c-1e099ce0860a',
+    debug: 3
+  });
+  peer.on('open', function() {
+    $('#my-id').text(peer.id);
+  });
+  peer.on('error', function(err) {
+    console.log(err.message);
+  });
+  peer.on('disconnected', function() {});
 
-function removeVideo(peerId) {
-  $('#their-video').get(0).srcObject = undefined;
-}
+  function addVideo(call, stream) {
+    $('#their-video').get(0).srcObject = stream;
+  }
 
-function setupMakeCallUI() {
-  $('#make-call').show();
-  $('#end-call').hide();
-}
+  function removeVideo(peerId) {
+    $('#their-video').get(0).srcObject = undefined;
+  }
 
-function setupEndCallUI() {
-  $('#make-call').hide();
-  $('#end-call').show();
-}
+  function setupMakeCallUI() {
+    $('#make-call').show();
+    $('#end-call').hide();
+  }
 
-function setupCallEventHandlers(call) {
-  if (existingCall) {
+  function setupEndCallUI() {
+    $('#make-call').hide();
+    $('#end-call').show();
+  }
+
+  function setupCallEventHandlers(call) {
+    if (existingCall) {
+      existingCall.close();
+    };
+    existingCall = call;
+    call.on('stream', function(stream) {
+      addVideo(call, stream);
+      setupEndCallUI();
+      $('#thir-id').text(call.remoteId);
+    });
+    call.on('close', function() {
+      removeVideo(call.remoteId);
+      setupMakeCallUI();
+    });
+  }
+
+  $('#make-call').submit(function(e) {
+    e.preventDefault();
+    const call = peer.call($('#callto-id').val(), localStream);
+    setupCallEventHandlers(call);
+  });
+  $('#end-call').click(function() {
     existingCall.close();
-  };
-  existingCall = call;
-  call.on('stream', function(stream) {
-    addVideo(call, stream);
-    setupEndCallUI();
-    $('#thir-id').text(call.remoteId);
+  })
+  peer.on('call', function(call) {
+    call.answer(localStream);
+    setupCallEventHandlers(call);
   });
-  call.on('close', function() {
-    removeVideo(call.remoteId);
-    setupMakeCallUI();
+  peer.on('close', function() {
+    existingCall.close();
   });
-}
-
-$('#make-call').submit(function(e) {
-  e.preventDefault();
-  const call = peer.call($('#callto-id').val(), localStream);
-  setupCallEventHandlers(call);
-});
-$('#end-call').click(function() {
-  existingCall.close();
-})
-peer.on('call', function(call) {
-  call.answer(localStream);
-  setupCallEventHandlers(call);
-});
-peer.on('close', function() {
-  existingCall.close();
 });
