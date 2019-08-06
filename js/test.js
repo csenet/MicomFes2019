4ea0707dc5d512febf030250629f6288cc13921e
kouichihirachi <kouichi.hirachi@gmail.com>
2019-08-06 10:54:09 +0900

    Index作成

--- js/test.js
+++ js/test.js
@@ -1,65 +1,83 @@
 $(function() {
   let localStream = null;
   let peer = null;
-
   let existingCall = null;
-  const screenshare = ScreenShare.create({
-    debug: true
-  });
-  if (screenshare.isScreenShareAvailable() === false) {
-    alert('Screen Share cannot be used. Please install the Chrome extension.');
-    return;
+  function stopScreen(){
+    screenshare.stop();
   }
-  screenshare.start({
-      width: $('#Width').val(),
-      height: $('#Height').val(),
-      frameRate: $('#FrameRate').val(),
+  function stopCamera(){
+    stream.getVideoTracks().forEach((track) => {
+      track.stop();
+    });
+    stream.getAudioTracks().forEach((track) => {
+      track.stop();
+    });
+  }
+
+  function screensharing() {
+    //画面共有
+    const screenshare = ScreenShare.create({
+      debug: true
+    });
+    if (screenshare.isScreenShareAvailable() === false) {
+      alert('Chorome拡張機能を追加してください');
+      return;
+    }
+    screenshare.start({
+        width: $('#Width').val(),
+        height: $('#Height').val(),
+        frameRate: $('#FrameRate').val(),
+      })
+      .then(stream => {
+        $('#my-video')[0].srcObject = stream;
+
+        if (existingCall !== null) {
+          const peerid = existingCall.peer;
+          existingCall.close();
+          const call = peer.call(peerid, stream);
+          step3(call);
+        }
+        localStream = stream;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  function camerastream() {
+    //カメラ映像配信
+    let media = navigator.mediaDevices.getUserMedia({
+      video: true, audio: true
     })
     .then(stream => {
-      $('#my-video')[0].srcObject = stream;
-
-      if (existingCall !== null) {
-        const peerid = existingCall.peer;
-        existingCall.close();
-        const call = peer.call(peerid, stream);
-        step3(call);
-      }
+      // Success
+      $('#my-video').get(0).srcObject = stream;
       localStream = stream;
     })
     .catch(error => {
-      console.log(error);
+      // Error
+      console.error('mediaDevice.getUserMedia() error:', error);
+      return;
     });
-  var result = screenshare.isScreenShareAvailable();
-  /*
-  let media = navigator.mediaDevices.getUserMedia({
-    video: {
-      mandatory: {
-        choromeMediaSource: 'desktop',
-        choromeMediaSourceId: ''
-      }
-    },
-    video: true,
-    audio: true
+  }
+
+  $('#changeCamera').click(function(){
+    camerastream();
   });
 
-  media.then(function(stream) {
-    // Success
-    $('#my-video').get(0).srcObject = stream;
-    localStream = stream;
+  $('#changeScreen').click(function(){
+    screensharing();
   });
-  media.catch(function(error) {
-    // Error
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
-  });
-  */
+
   peer = new Peer({
     key: '4852b666-0601-4aaf-b74c-1e099ce0860a',
     debug: 3
   });
+
   peer.on('open', function() {
     $('#my-id').text(peer.id);
   });
+  
   peer.on('error', function(err) {
     console.log(err.message);
   });
