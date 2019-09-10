bf03faa8fb45af1c913476ff5f0634cb8c11dc84
kouichihirachi <kouichi.hirachi@gmail.com>
2019-09-10 09:37:52 +0900

    スライド追加

--- Workshop/print.php
+++ Workshop/print.php
@@ -5,8 +5,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+  integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+  crossorigin="anonymous"></script>
 
   <script src="./js/jquery-barcode.min.js"></script>
   <script src="./js/jquery-barcode.js"></script>
@@ -14,15 +18,17 @@
   <title>受付票プリント</title>
   <script>
     //自動プリント
+    /*
     $(window).on('load', function() {
       setTimeout(function() {
         window.print();
         window.close();
       }, 200);
     })
+    */
     //バーコード表示
     $(function(){
-       $("#barcode").barcode("<?php echo $_GET["code"]?>", "ean13", {barWidth:2,barHeight:30});
+       $("#barcode").barcode("1000000000000", "ean13", {barWidth:2,barHeight:30});
     });
   </script>
 </head>
--- js/slide.js
+++ js/slide.js
@@ -1,9 +1,9 @@
 $(function () {
     $('.slider-items').slick({
-        autoplay: true, //自動再生
-        dots: true, //ドットのナビゲーションを表示
-        infinite: true, //スライドのループ有効化
-        speed: 1000, //切り替えのスピード（小さくすると速くなる）
-        fade: true, //フェードの有効化
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
     });
 });
